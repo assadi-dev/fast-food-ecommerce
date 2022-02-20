e220e241324319462d897b5b05ee0e509c9f6fe2
Assadi <assadi.halifa@gmail.com>
2022-02-20 16:07:57 +0100

    add section

--- client/pages/Section/Services/index.js
+++ client/pages/Section/Services/index.js
@@ -1,7 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas, faHat } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
 import React from "react";
 import styles from "./styles.module.css";
 
 function Services() {
+  library.add(far, fas);
   return (
     <section>
       <h2 className={styles.title}>Commande en ligne</h2>
@@ -9,7 +15,13 @@ function Services() {
         <div>sur place ou emporté</div>
         <div>sur place ou emporté</div>
         <div>sur place ou emporté</div>
-        <div>sur place ou emporté</div>
+        <div>
+          <p>
+            <FontAwesomeIcon icon={["far", "hat-cowboy"]} size="2x" />
+            <FontAwesomeIcon icon={["far", "chevron-up"]} size="2x" />
+          </p>
+          sur place ou emporté
+        </div>
       </div>
     </section>
   );
